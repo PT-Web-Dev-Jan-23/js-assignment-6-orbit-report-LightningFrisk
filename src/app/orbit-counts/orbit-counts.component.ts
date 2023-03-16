4c7f5544381eb88bf7464671b26d9b34d3272f19
LightningFrisk <bmxcaleb@gmail.com>
2023-03-15 21:58:35 -0500

    finishing project
    finishing project

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -15,13 +15,11 @@ export class OrbitCountsComponent implements OnInit {
   ngOnInit() {
   }
 
-  countByType(type: string): number {
+  countSatellites(type: string): number {
 	let count = 0;
 	if (this.satellites) {
 	  for (let i = 0; i < this.satellites.length; i++) {
-		 if (this.satellites[i].type === type) {
-			count++;
-		 }
+		count++;
 	  }
 	}
 	return count;
